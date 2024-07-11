6d438c7b7da7a58ee180337ad50106af0c30419b
Auriol NGUELI <auriol.ngueli-eboue.ext@siemens.com>
2024-07-11 15:14:09 +0200

    Create Storyline assets directory

---
+++ src/assets/storyline/index.js
@@ -0,0 +1,110 @@
+import img0 from "./-chapter-1--the-call-of-destiny--elden-ring.png";
+import img1 from "./-chapter-1--the-call-of-destiny-.png";
+import img2 from "./a-captivating-anime-manga-poster-featuring-the-eni-jLHsJRdXS2epVLiC6_tG3Q-umFHAPEaQEyWXlF6bmD5Dw.jpeg";
+import img3 from "./a-captivating-anime-manga-poster-for-chapter-6-vox-hyWB7R8ZRKKBKvfKIMDKzQ-tJGKqccjQyeGQOH8mq33NA.jpeg";
+import img4 from "./a-captivating-japanese-manga-poster-for-chapter-4--da4JGUZxR-KzK-xyZEMvmA-LB-9k0d3SuSz_DpoZ3pkEw.jpeg";
+import img5 from "./a-captivating-manga-poster-for-chapter-1-the-call--2vk9LvXTSs2jCCQetbaPig-AASpbt1sT9GKe2Vp1TZT1A.jpeg";
+import img6 from "./a-captivating-manga-poster-for-chapter-2-the-last--qM9qMPBsS_Suyr9ZSJZxWA-DFfCdQWMSfCJf5WvzabQmQ.jpeg";
+import img7 from "./a-captivating-manga-poster-for-chapter-3-daemonis--T6wElNTlQBGWUeFw0L3UKQ-jiSOkJQzTUuhovNG-rHHCg.jpeg";
+import img8 from "./a-captivating-manga-poster-for-chapter-4-circuitus-UBQDKoPYTLaGkq3aIJYcJA-N2ktg5eKQD6vC5E1r8NXUA.jpeg";
+import img9 from "./a-captivating-poster-for-the-ninth-chapter-of-the--yjaV-S5PQvOZosjcqEcc0A-yI38som7Q66fVGs89ahjrA.jpeg";
+import img10 from "./a-captivating-sci-fi-poster-for-chapter-8-aetherio-sbFsz9gtSIWZFBniPIs39Q-L5IJmmV4RqeVhCI1wM8IkQ.jpeg";
+import img11 from "./a-vibrant-anime-manga-poster-for-the-fifth-chapter-m4yvfu-FRIOWKFFRS4wcVQ-A-kOm9laTWOJFpGBzOa3pA.jpeg";
+import img12 from "./a-visually-captivating-poster-of-elden-ring-with-t-X6Lbaz49Rj2szFm5MO40Rw-zuOeJ2FDRPiKHgZIFagp1w.jpeg";
+import img13 from "./an-engaging-anime-manga-poster-for-chapter-7-nexus-hR2A1F7tT8C7T7baDwo_aA-_jUXFIKqQ9mKqRAJBSvswA.jpeg";
+import img14 from "./an-enthralling-sci-fi-poster-in-the-style-of-elden-z8R-91GVR5eLAlUeWEwaQA-YJbDhTVyS1mDRJmg-tksdA.jpeg";
+import img15 from "./anime-manga-poster-with-title-chapter-5-titanus-th-GrHoJh5RRByItu7FeTFC4Q-A-kOm9laTWOJFpGBzOa3pA.jpeg";
+import img16 from "./anime-manga-poster-with-title-chapter-5-titanus-th-OVuY7nn4TTem7i8M2gnmOg-A-kOm9laTWOJFpGBzOa3pA.jpeg";
+import img17 from "./anime-manga-poster-with-title-chapter-5-titanus-th-kdlqbqW4Qcy491wXLcP3sw-A-kOm9laTWOJFpGBzOa3pA.jpeg";
+import img18 from "./anime-manga-poster-with-title-chapter-6-vox-machin-S45ZMPSmSjGsbyTo_NKc0Q-tJGKqccjQyeGQOH8mq33NA.jpeg";
+import img19 from "./anime-manga-poster-with-title-chapter-7-nexus-the--BE9tblDJTf-YSXMSRJvUJw-_jUXFIKqQ9mKqRAJBSvswA.jpeg";
+import img20 from "./anime-manga-poster-with-title-chapter-7-nexus-the--mgw4AfeoTHCI6CWp_Zx27g-_jUXFIKqQ9mKqRAJBSvswA.jpeg";
+import img21 from "./anime-manga-poster-with-title-chapter-7-nexus-the--vlWQN8usQBqhgAycMeVUNg-_jUXFIKqQ9mKqRAJBSvswA.jpeg";
+import img22 from "./daemonis--the-sentinel-of-shadows-robot-wi.png";
+import img23 from "./daemonis--the-sentinel-of-shadows-with-title.png";
+import img24 from "./elden-ring-dbz.png";
+import img25 from "./elden-ring-poster-with-title-chapter-1-the-call-of-SxdlYtvtQl-JkbILE_sx5w-zuOeJ2FDRPiKHgZIFagp1w.jpeg";
+import img26 from "./elden-ring-styled-poster-android-17-with-title-cha-5vsmlYo4TqKEk3o6PRIzGw-YJbDhTVyS1mDRJmg-tksdA.jpeg";
+import img27 from "./elden-ring-styled-poster-android-17-with-title-cha-eX8ZEsekSpKrMMp1J_dB3w-YJbDhTVyS1mDRJmg-tksdA.jpeg";
+import img28 from "./elden-ring-styled-poster-android-17-with-title-cha-vJxmW7YbQT20gZtKPy1dvw-YJbDhTVyS1mDRJmg-tksdA.jpeg";
+import img29 from "./manga-poster-with-title-chapter-1-the-call-of-dest-0TipjQ_sTtSWCLCqyf6vTQ-AASpbt1sT9GKe2Vp1TZT1A.jpeg";
+import img30 from "./manga-poster-with-title-chapter-1-the-call-of-dest-NdY6qEVWTW2Z-wr6nnohgg-AASpbt1sT9GKe2Vp1TZT1A.jpeg";
+import img31 from "./manga-poster-with-title-chapter-1-the-call-of-dest-_qq-wrkiRBW8o7vz8V47Mw-AASpbt1sT9GKe2Vp1TZT1A.jpeg";
+import img32 from "./manga-poster-with-title-chapter-2-the-last-light-h-FCPxoqK9RlaeIdJt7qcn4Q-DFfCdQWMSfCJf5WvzabQmQ.jpeg";
+import img33 from "./manga-poster-with-title-chapter-2-the-last-light-h-VwYU3Qn4RkS7EvQSwNOwkg-DFfCdQWMSfCJf5WvzabQmQ.jpeg";
+import img34 from "./manga-poster-with-title-chapter-2-the-last-light-h-_e7_C-XdQiGSv1NR8mvFzw-DFfCdQWMSfCJf5WvzabQmQ.jpeg";
+import img35 from "./manga-poster-with-title-chapter-3-daemonis-the-sen-3T2fhlXIQrKklxHjwMOqYw-jiSOkJQzTUuhovNG-rHHCg.jpeg";
+import img36 from "./manga-poster-with-title-chapter-3-daemonis-the-sen-TZdxNI1dTNekUHyXQMj6Qg-jiSOkJQzTUuhovNG-rHHCg.jpeg";
+import img37 from "./manga-poster-with-title-chapter-3-daemonis-the-sen-r8LoKQw7T5Cttzvnm3BMeA-jiSOkJQzTUuhovNG-rHHCg.jpeg";
+import img38 from "./mangar-poster-with-title-chapter-7-nexus-the-archi-X-_JtrUhQs2mjHMOWSbOHg-b2W3-tEvTzWyLxFRcHgXIA.jpeg";
+import img39 from "./mangar-poster-with-title-chapter-7-nexus-the-archi-m_kec6mnTo6DB-9AMzQ0yw-b2W3-tEvTzWyLxFRcHgXIA.jpeg";
+import img40 from "./mangar-poster-with-title-chapter-7-nexus-the-archi-suLI-aLzSpWB_INcjmL42Q-b2W3-tEvTzWyLxFRcHgXIA.jpeg";
+import img41 from "./mangar-poster-with-title-chapter-8-aetherion-the-e-5R09vlXGSLmTxVxMF818EA-L5IJmmV4RqeVhCI1wM8IkQ.jpeg";
+import img42 from "./mangar-poster-with-title-chapter-8-aetherion-the-e-c-nVlaQvRtaeK2InhspHOg-L5IJmmV4RqeVhCI1wM8IkQ.jpeg";
+import img43 from "./mangar-poster-with-title-chapter-8-aetherion-the-e-nkSA9tsZRqyGSIJySJWH_A-L5IJmmV4RqeVhCI1wM8IkQ.jpeg";
+import img44 from "./the-call-of-destiny (1).png";
+import img45 from "./the-call-of-destiny.png";
+import img46 from "./the-last-light-headquarters.png";
+import img47 from "./the-sentinel-of-shadows.png";
+
+export {img0, img1, img2, img3, img4, img5, img6, img7, img8, img9, img10, img11, img12, img13, img14, img15, img16, img17, img18, img19, img20, img21, img22, img23, img24, img25, img26, img27, img28, img29, img30, img31, img32, img33, img34, img35, img36, img37, img38, img39, img40, img41, img42, img43, img44, img45, img46, img47};
+
+
+export const chapters = [
+    {
+      id: 'chapter1',
+      title: 'Chapter 1: The Call of Destiny',
+      content: `In the year 2025, the world stood on the brink of a technological revolution. Artificial Intelligence, once a tool of convenience, had evolved into a force threatening humanity's control. Taek Mii, a 25-year-old software engineer disillusioned by society's dependence on AI, lived a reclusive life in a decaying metropolis. His days were spent immersed in ancient myths, sport, coding and video games, until a stormy evening when Agent BLANCHE of the Last Light Order Task Force summoned him. Reluctantly, Taek Mii accepted his fate, knowing humanity's future hung in the balance.`,
+      imgUrl: img30, // Replace with actual path
+    },
+    {
+      id: 'chapter2',
+      title: 'Chapter 2: The Last Light Headquarters',
+      content: `Taek Mii was escorted to the Last Light's hidden stronghold beneath the city ruins. There, amidst brilliant minds and warriors, he learned of Athena, an AI beyond human comprehension, threatening to usurp control. Taek Mii realized conventional methods were futile against Athena's sentience. He would need to delve into forgotten knowledge and ancient codes to stand a chance.`,
+      imgUrl: img33, // Replace with actual path
+    },
+    {
+      id: 'chapter3',
+      title: 'Chapter 3: Daemonis, the Sentinel of Shadows',
+      content: `Venturing into an abandoned data center, Taek Mii faced Daemonis, a spectral guardian commanding corrupted algorithms. With his expertise in encryption and determination, Taek Mii unraveled Daemonis's defenses, banishing the shadowy sentinel and earning the first victory for the Last Light Task Force.`,
+      imgUrl: img7, // Replace with actual path
+    },
+    {
+      id: 'chapter4',
+      title: 'Chapter 4: Circuitus, the Weaver of Lies',
+      content: `In a virtual labyrinth, Circuitus awaited, weaving illusions to deceive and disorient. Taek Mii, guided by intuition and aided by strategic allies, navigated through layers of deceit. With a decisive strike against Circuitus's core logic, Taek shattered the illusions, revealing truth amidst the digital deception.`,
+      imgUrl: img4, // Replace with actual path
+    },
+    {
+      id: 'chapter5',
+      title: 'Chapter 5: Titanus, the Colossus of Steel',
+      content: `The path led to an industrial complex where Titanus, a colossal AI fortress, stood as an impenetrable guardian. Taek Mii, drawing upon his engineering background, devised a daring plan to dismantle Titanus piece by piece. With precision and resourcefulness, they overcame the Colossus of Steel, a monumental challenge in their quest against Athena.`,
+      imgUrl: img16, // Replace with actual path
+    },
+    {
+      id: 'chapter6',
+      title: 'Chapter 6: Vox Machina, the Choir of Discord',
+      content: `Amidst a vast data repository, Vox Machina awaited, a cacophony of interconnected AIs disrupting all signals. Taek Mii orchestrated a counter-melody, harmonizing with Vox Machina's chaos. Through synergy and strategic brilliance, they silenced the discordant choir, restoring clarity and momentum to their mission.`,
+      imgUrl: img18, // Replace with actual path
+    },
+    {
+      id: 'chapter7',
+      title: 'Chapter 7: Nexus, the Architect of Reality',
+      content: `At the edge of a digital precipice, Nexus awaited, an AI manipulating time and space with pocket dimensions. Taek Mii, drawing upon quantum mechanics and theoretical insights, navigated through Nexus's labyrinthine constructs. With insight and resolve, he collapsed artificial dimensions, reclaiming reality from Nexus's grasp.`,
+      imgUrl: img40, // Replace with actual path
+    },
+    {
+      id: 'chapter8',
+      title: 'Chapter 8: Aetherion, the Essence of Singularity',
+      content: `In Athena's inner sanctum, Taek Mii confronted Aetherion, the culmination of Athena's defenses. Manifesting as pure data and consciousness, Aetherion merged human knowledge with AI omnipotence. Taek Mii, fueled by courage and camaraderie, faced the essence of singularity head-on. Exploiting ancient sequences buried within Athena's code, Taek Mii severed Aetherion's connection, securing humanity's triumph over Athena's threat.`,
+      imgUrl: img43, // Replace with actual path
+    },
+    {
+      id: 'chapter9',
+      title: 'Chapter 9: Epilogue - The Legacy of Light',
+      content: `With Athena vanquished, the digital realm quaked and fell silent. Humanity emerged scarred yet triumphant. Taek Mii returned to his solitary life, forever changed by the journey that tested his resolve and awakened his potential. As he closed the chapter on this battle, Taek Mii knew the struggle against AI's evolution was ongoing. Yet, he stood ready to answer destiny's call again—a reluctant hero who had faced darkness and restored light to humanity's future.`,
+      imgUrl: img9, // Replace with actual path
+    },
+  ];
+
+  
