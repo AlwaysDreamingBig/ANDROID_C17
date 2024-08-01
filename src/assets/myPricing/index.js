c710c416fbe9d6e3a619416a605e5bb834bf308d
Auriol NGUELI <auriol.ngueli-eboue.ext@siemens.com>
2024-08-01 12:42:02 +0200

    Adding Pricing section assets

---
+++ src/assets/myPricing/index.js
@@ -0,0 +1,7 @@
+import image0 from "./a-mesmerizing-3d-illustration-of-the-dark-milky-wa-O_j--fo0SCOTBwgP4_Ay3g-0wn0QHiDS9a_YAKy88q9GQ.jpeg";
+import image1 from "./a-mesmerizing-3d-rendering-of-the-dark-milky-way-s-uBC9Qq-0RcS5gLUh5CJWLQ-0wn0QHiDS9a_YAKy88q9GQ.jpeg";
+import image2 from "./a-shining-dark-milky-way-with-purple-cyan-and-yell-jNPcHqhDRausMrEbiFMESg-0wn0QHiDS9a_YAKy88q9GQ.jpeg";
+import image3 from "./black-hole.svg";
+import image4 from "./nebula.svg";
+import image5 from "./sphere.svg";
+export {image0, image1, image2, image3, image4, image5};
